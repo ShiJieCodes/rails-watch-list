9f0945d1380a0d3ea7a031811497d2a9c1079ff4
Darren Lim <shijielim@gmail.com>
2023-06-13 06:41:29 +0800

    Updated

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,9 +8,27 @@ class ListsController < ApplicationController
   def show
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
